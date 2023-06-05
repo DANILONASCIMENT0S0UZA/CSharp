758157322e9c9a83a2e48cfc4dadd42975f05e5b
DANILO NASCIMENTO SOUZA <danilo.nsouza1@senacsp.edu.br>
2023-06-05 14:44:24 -0300

    Atualizado

--- Colecoes/Queue.cs
+++ Colecoes/Queue.cs
@@ -8,6 +8,30 @@ namespace CursoCSharp.Colecoes {
     class Queue {
         public static void Executar() {
 
+            var fila = new Queue<string>();
+
+            fila.Enqueue("Milo");
+            fila.Enqueue("Wanderely");
+            fila.Enqueue("Cleyton");
+            fila.Enqueue("Agatha");
+            fila.Enqueue("Triz");
+
+            //Pegando o primeiro da fila
+
+            Console.WriteLine(fila.Peek());
+
+            //contando a fila
+
+            Console.WriteLine(fila.Count);
+
+            //remover a primeira pessa da fila
+            Console.WriteLine(fila.Dequeue());
+            Console.WriteLine(fila.Count);
+
+            //verificando quantos pessoas na fila
+            foreach(var pessoa in fila) {
+                Console.WriteLine(pessoa);
+            }
         }
     }
 }
